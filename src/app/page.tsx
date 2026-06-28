8386c1ae4c882170cd55285ca1fe88d588522165
eshubaag-source <eshubaag@gmail.com>
2026-06-28 16:37:30 +0530

    Update page.tsx
    login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 "use client";
-
-import VehicleFeesPage from "./dashboard/vehicle-fees/page";
+import LoginPage from "./login/page";
 
 export default function Home() {
   return (
     <div style={{ padding: "2rem" }}>
-      <h1 style={{ fontSize: "2rem", marginBottom: "0rem" }}>Vehicle Fees Overview</h1>
-      <hr className="horizontalBar" />
-      <VehicleFeesPage />
+      <LoginPage/>
     </div>
   );
 }
