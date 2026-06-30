05d592abb36c782f38e76c67966d3481bd37a1d9
eshubaag-source <eshubaag@gmail.com>
2026-06-30 11:01:41 +0530

    Add files via upload

---
+++ check_db_users.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({ path: '.env.local' });
+
+mongoose.connect(process.env.MONGODB_URI, { bufferCommands: false }).then(async () => {
+  const db = mongoose.connection.db;
+  const admins = await db.collection('admins').find({}).toArray();
+  const teachers = await db.collection('teachers').find({}).toArray();
+  admins.map(a => ({ id: a._id, username: a.username, email: a.email }));
+  teachers.map(t => ({ id: t._id, name: t.name, email: t.email }));
+  process.exit(0);
+});
