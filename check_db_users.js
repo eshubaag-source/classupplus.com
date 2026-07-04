4b2dce5a491d3c9432e09cbe91b10621df2cb7cf
eshubaag-source <eshubaag@gmail.com>
2026-07-04 10:29:30 +0530

    Update check_db_users.js

--- check_db_users.js
+++ check_db_users.js
@@ -6,7 +6,7 @@ mongoose.connect(process.env.MONGODB_URI, { bufferCommands: false }).then(async 
   const db = mongoose.connection.db;
   const admins = await db.collection('admins').find({}).toArray();
   const teachers = await db.collection('teachers').find({}).toArray();
-  admins.map(a => ({ id: a._id, username: a.username, email: a.email }));
-  teachers.map(t => ({ id: t._id, name: t.name, email: t.email }));
+  console.log('ADMINS:', admins.map(a => ({ id: a._id, username: a.username, email: a.email })));
+  console.log('TEACHERS:', teachers.map(t => ({ id: t._id, name: t.name, email: t.email })));
   process.exit(0);
 });
