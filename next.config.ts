f0865550b26e8b3c12057f962643e679d0076ad1
eshubaag-source <eshubaag@gmail.com>
2026-07-09 11:29:43 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    // यह Vercel पर बिल्ड के दौरान ESLint एरर को अनदेखा कर देगा
-    ignoreDuringBuilds: true,
-  },
-  // अगर टाइपस्क्रिप्ट के कारण भी दिक्कत आए तो इसे भी जोड़ सकते हैं:
-  typescript: {
-    ignoreBuildErrors: true,
-  }
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
