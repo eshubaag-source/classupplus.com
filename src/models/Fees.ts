6f69874553f1b54149244fa8d5b1c87f7bf8218c
eshubaag-source <eshubaag@gmail.com>
2026-07-25 22:27:46 +0530

    Update Fees.ts

--- src/models/Fees.ts
+++ src/models/Fees.ts
@@ -5,11 +5,13 @@ const FeesSchema = new mongoose.Schema({
   studentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Student', required: true },
   amount: { type: Number, required: true },
   status: { type: String, enum: ['Paid', 'Pending'], default: 'Pending' },
+  category: { type: String, enum: ['Class', 'Hostel'], default: 'Class' },
   month: { type: String, required: true }, // e.g., "January 2024"
   paidDate: { type: Date },
   description: { type: String },
   utr: { type: String },
   lastyear: { type: String },
+  lasyearamount: { type: String },
   createdAt: { type: Date, default: Date.now },
 });
 
