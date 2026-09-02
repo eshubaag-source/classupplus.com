62b3ee128f41aa6fab18456ab85d4223ce9534d0
eshubaag-source <eshubaag@gmail.com>
2026-09-02 14:18:01 +0530

    Update route.ts

--- src/app/api/fees/route.ts
+++ src/app/api/fees/route.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextResponse } from 'next/server';
 import dbConnect from '@/lib/db';
 import Fees from '@/models/Fees';
